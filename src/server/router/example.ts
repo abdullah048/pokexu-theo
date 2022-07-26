f205fbe86169d48ef31f5e5c02235d0525482899
IB1148981 <103644657+IB1148981@users.noreply.github.com>
2022-07-26 18:06:00 +0500

    initial commit-vercel

--- src/server/router/example.ts
+++ src/server/router/example.ts
@@ -1,21 +1,15 @@
 import { createRouter } from "./context";
 import { z } from "zod";
 
-export const exampleRouter = createRouter()
-  .query("hello", {
-    input: z
-      .object({
-        text: z.string().nullish(),
-      })
-      .nullish(),
-    resolve({ input }) {
-      return {
-        greeting: `Hello ${input?.text ?? "world"}`,
-      };
-    },
-  })
-  .query("getAll", {
-    async resolve({ ctx }) {
-      return await ctx.prisma.example.findMany();
-    },
-  });
+import { PokemonClient } from "pokenode-ts";
+
+export const exampleRouter = createRouter().query("get-pokemon-by-id", {
+	input: z.object({
+		id: z.number(),
+	}),
+	async resolve({ input }) {
+		const api = new PokemonClient();
+		const pokemon = await api.getPokemonById(input.id);
+		return { name: pokemon.name, sprites: pokemon.sprites.front_default };
+	},
+});
